ffa45e19bf898e19ce8eb0bf22cd2bcd5da1ca25
ShevchukAndrii <andrefanchik@gmail.com>
2023-06-11 22:35:29 +0300

    for check

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,27 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const array = [];
-for (let i = 0; i < galleryItems.length; i += 1) {
-    const item = galleryItems[i];
-    const liEl = document.createElement('li');
-    liEl.classList.add('gallery__item');
-    const aEl = document.createElement('a');
-    aEl.classList.add('gallery__link');
-    aEl.href = item.original;
-    liEl.prepend(aEl);
-    const imgEl = document.createElement('img');
-    imgEl.classList.add('gallery__image');
-    imgEl.src = item.preview;
-    imgEl.alt = item.description;
-    aEl.prepend(imgEl);
-    array.push(liEl);
-}
+const createItem = ({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+};
+
+const createItemForUl = galleryItems.map(createItem).join('');
 const ulEl = document.querySelector(".gallery");
-ulEl.append(...array);
+ulEl.insertAdjacentHTML('beforeend', createItemForUl);
 
 var lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: '250', 
 })
 
-console.log(galleryItems);
